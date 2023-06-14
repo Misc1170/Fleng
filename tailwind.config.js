739fc39892cc6929f880a653234e2fc368be65c4
Misc1170 <sloopy1170@gmail.com>
2023-06-14 15:02:19 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./assets/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'D9D9D9': '#D9D9D9',
+      '767676': '#767676',
+      '00C7C7': '#00C7C7',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
